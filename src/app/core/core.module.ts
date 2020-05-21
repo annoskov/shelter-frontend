e8cc8e3e2dc7798f5f79b723242061c2be132fd2
antonnoskov <anoskov@dm-matrix.com>
2020-05-21 11:33:22 +0300

    fix(CoreModule) - fixed imports

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,11 +3,8 @@ import { CommonModule } from '@angular/common';
 import { EntryComponent } from './entry/entry.component';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
-import { SidenavComponent } from './shell/sidenav/sidenav.component';
 import { ContentComponent } from './shell/content/content.component';
-import { HeaderComponent } from './shell/header/header.component';
 import { ShellComponent } from './shell/shell.component';
-import { MainMenuComponent } from './shell/main-menu/main-menu.component';
 import { AuthenticationGuard } from './authentication/authentication.guard';
 import { AuthenticationService } from './authentication/authentication.service';
 
@@ -15,10 +12,7 @@ import { AuthenticationService } from './authentication/authentication.service';
   declarations: [
     EntryComponent,
     ShellComponent,
-    HeaderComponent,
     ContentComponent,
-    SidenavComponent,
-    MainMenuComponent
   ],
   imports: [
     CommonModule,
